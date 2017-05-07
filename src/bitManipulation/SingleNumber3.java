abe8904e0e35c0f9d3fe13bddafa227f338c5f1c
ytian90 <tianyu.ouc@gmail.com>
2017-05-06 22:29:55 -0700

    update

--- src/bitManipulation/SingleNumber3.java
+++ src/bitManipulation/SingleNumber3.java
@@ -5,25 +5,28 @@ package bitManipulation;
  * @since Aug 29, 2015
  */
 public class SingleNumber3 {
+	// Time: O(n) Space: O(1)
 	public static int[] singleNumber(int[] nums) {
 		// Pass 1:
 		// Get the XOR of the two numbers we need to find
 		int diff = 0;
-		for (int num : nums) {
-			diff ^= num;
+		for (int n : nums) {
+			diff ^= n;
 		}
 		// Get its last set bit
+		//  53 --> 00110101
+	    // -53 --> 11001010 + 1 = 11001011
 		diff &= -diff;
 		
 		// Pass 2:
-		int[] rets = {0, 0}; // this array stores the two numbers we will return
-		for (int num : nums) {
-			if ((num & diff) == 0) // the bit is not set
-				rets[0] ^= num;
+		int[] res = {0, 0}; // this array stores the two numbers we will return
+		for (int n : nums) {
+			if ((n & diff) == 0) // the bit is not set
+				res[0] ^= n;
 			else // the bit is set
-				rets[1] ^= num;
+				res[1] ^= n;
 		}
-		return rets;
+		return res;
 	}
 	
 	public static void main(String[] args) {
