73f73b361b8666f418d8afbf55cd185f280d23ac
ytian90 <tianyu.ouc@gmail.com>
2017-05-16 00:07:37 -0700

    Tue

--- src/hashTable/TopKFrequentElements.java
+++ src/hashTable/TopKFrequentElements.java
@@ -2,8 +2,10 @@ package hashTable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * 347. Top K Frequent Elements
@@ -25,14 +27,35 @@ public class TopKFrequentElements {
 			}
 			bucket[freq].add(key);
 		}
-		List<Integer> result = new ArrayList<>();
-		for (int i = bucket.length - 1; i >= 0 && result.size() < k; i--) {
+		List<Integer> res = new ArrayList<>();
+		for (int i = bucket.length - 1; i >= 0 && res.size() < k; i--) {
 			if (bucket[i] != null) {
-				result.addAll(bucket[i]);
+				res.addAll(bucket[i]);
 			}
 		}
-        return result;
+        return res;
     }
+	
+	public List<Integer> topKFrequent2(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+		TreeMap<Integer, List<Integer>> freqMap = new TreeMap<>();
+		for (int n : nums) {
+			map.put(n, map.getOrDefault(n, 0) + 1);
+		}
+		for (int num: map.keySet()) {
+			int freq = map.get(num);
+			if (!freqMap.containsKey(freq)) {
+				freqMap.put(freq, new LinkedList<>());
+			}
+			freqMap.get(freq).add(num);
+		}
+		List<Integer> res = new ArrayList<>();
+		while (res.size() < k) {
+			Map.Entry<Integer, List<Integer>> entry = freqMap.pollLastEntry();
+			res.addAll(entry.getValue());
+		}
+		return res;
+	}
 
 	public static void main(String[] args) {
 		TopKFrequentElements t = new TopKFrequentElements();
